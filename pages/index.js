eb379eb2182f7fed953b1f281b5de96c781bf116
Berat MyClub <b.uzun@myclub.dance>
2022-12-12 14:27:02 +0100

    fix: Font and Triangle

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import jsPDF from "jspdf";
 import { useState } from "react";
 import styled from "styled-components";
+import { callAddFont } from "../styles/fonts/SairaSemiCondensed-Regular-normal";
+import { callAddFontBold } from "../styles/fonts/SairaSemiCondensed-Bold-normal";
 
 const Upload = styled.div`
   width: 15rem;
@@ -26,10 +28,13 @@ export default function Home() {
     setSelectedImages(imagesArray);
   };
 
+  jsPDF.API.events.push(["addFonts", callAddFont]);
+  jsPDF.API.events.push(["addFonts", callAddFontBold]);
+
   const generatePDF = () => {
     var doc = new jsPDF("p", "px", "a5");
     var pageWidth = 315;
-    var pageHeight = 416;
+    var pageHeight = 417;
     var lines = doc.splitTextToSize(
       "Scanne jetzt deinen QR-Code für die schnelle und unkomplizierte Ein- und Auszahlung auf deine E-Wallet.",
       200
@@ -37,26 +42,41 @@ export default function Home() {
     doc.setFillColor("#243761");
     doc.rect(0, 0, pageWidth, 30, "F");
     doc.addImage("logo.png", "PNG", 122.5, 0, 70, 30);
-    doc.setFont('helvetica', 'bold')
-    doc.text(lines, 155, 100, 'center').splitTextToSize(50);
-    // doc.text("schnelle und unkomplizierte Ein- und", 60, 115);
-    // doc.text("Auszahlung auf deine E-Wallet", 75, 130);
+    doc.setFont("SairaSemiCondensed-Regular", "normal");
+    doc.text("Scanne jetzt deinen QR-Code für die", 68, 80);
+    doc.setFont("SairaSemiCondensed-Bold", "normal");
+    doc.text("schnelle", 63, 98);
+    doc.setFont("SairaSemiCondensed-Regular", "normal");
+    doc.text("und", 108, 98);
+    doc.setFont("SairaSemiCondensed-Bold", "normal");
+    doc.text("unkomplizierte Ein- und", 131, 98);
+    doc.text("Auszahlung", 79, 116);
+    doc.setFont("SairaSemiCondensed-Regular", "normal");
+    doc.text("auf deine E-Wallet.", 141, 116);
+    doc.setFillColor("#243761");
+    doc.triangle(142, 143, 172, 143, 157, 158, "FD");
     doc.setDrawColor("#243761");
     doc.setLineWidth(2);
     doc.rect(97.5, 170, 120, 120, "S");
     doc.addImage(`${selectedImages}`, "JPEG", 100.5, 172, 115, 115);
     doc.setFillColor("#243761");
     doc.rect(0, pageHeight, pageWidth, 30, "F");
+    doc.setFontSize(6);
+    doc.setTextColor("#fff");
+    doc.text("OKTOPAY Deutschland GmbH", 10, 426).splitTextToSize(50);
+    doc.text("Am Zirkus 2", 10, 431).splitTextToSize(50);
+    doc.text("10117 Berlin", 10, 436).splitTextToSize(50);
+    doc.text("Deutschland", 10, 441).splitTextToSize(50);
+
     doc.addImage("store.png", "PNG", 70, 280, 250, 250);
     doc.save("flyer.pdf");
-  
 
-    console.log(doc.getFontList())
+    console.log(doc.getFontList());
   };
   return (
     <div className="outter-container">
       <div className="config-container">
-        <img style={{maxWidth:"8rem"}} src="logo.png"/>
+        <img style={{ maxWidth: "8rem" }} src="logo.png" />
         <input
           onChange={onSelectedFile}
           type="file"
